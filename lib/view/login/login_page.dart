083c4a43218351c68cdd04bceba68c9aec895a7e
Mik255 <mikael20informatica@gmail.com>
2023-06-26 16:48:58 -0300

    background img and improvement page style

--- lib/view/login/login_page.dart
+++ lib/view/login/login_page.dart
@@ -6,11 +6,11 @@ import '../../models/user.dart';
 import '../designer_system/button_decoretor.dart';
 import '../designer_system/custom_text_field.dart';
 
-class LoginPage extends StatelessWidget {
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
+final TextEditingController emailController = TextEditingController();
+final TextEditingController passwordController = TextEditingController();
 
-  LoginPage({super.key});
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -25,98 +25,115 @@ class LoginPage extends StatelessWidget {
       user: user,
       helper: helper,
     );
-    return Scaffold(
-      backgroundColor: Colors.blue,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Padding(
-            padding: EdgeInsets.symmetric(vertical: 16.0),
-            child: CircleAvatar(
-              radius: 50,
-              backgroundColor: Colors.white70,
-              child: Icon(
-                Icons.monetization_on_outlined,
-                size: 50,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          Card(
-            margin: const EdgeInsets.all(16.0),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Form(
-                key: controller.formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/login_background.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Spacer(),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: const Wrap(
                   children: [
-                    TextFormField(
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      controller: emailController,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter some text';
-                        }
-                        if (!user.validateEmail()) {
-                          return 'Email is not valid';
-                        }
-                        return null;
-                      },
-                      decoration:
-                          CustomTextFieldDecorator().getInputDecorator('Email'),
-                    ),
-                    const SizedBox(height: 16.0),
-                    TextFormField(
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      controller: passwordController,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter some text';
-                        }
-                        if (!user.validatePassword()) {
-                          return 'password is not valid';
-                        }
-                        return null;
-                      },
-                      obscureText: true,
-                      decoration: CustomTextFieldDecorator()
-                          .getInputDecorator('Password'),
-                    ),
-                    const SizedBox(height: 16.0),
-                    AnimatedBuilder(
-                        animation: controller.message,
-                        builder: (context, child) {
-                          return Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              style: const TextStyle(
-                                color: Colors.red,
-                              ),
-                              controller.message.value,
-                            ),
-                          );
-                        }),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: SizedBox(
-                            height: 50,
-                            child: ElevatedButton(
-                              style: CustomButtonStyle().getStyle(),
-                              onPressed: controller.login,
-                              child: const Text('Enter'),
-                            ),
-                          ),
-                        ),
-                      ],
+                    Text(
+                      'Wallet Manager',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 45,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ],
                 ),
               ),
             ),
-          )
-        ],
+            Card(
+              margin: const EdgeInsets.all(16.0),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Form(
+                  key: controller.formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextFormField(
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        controller: emailController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          if (!user.validateEmail()) {
+                            return 'Email is not valid';
+                          }
+                          return null;
+                        },
+                        decoration: CustomTextFieldDecorator()
+                            .getInputDecorator('Email'),
+                      ),
+                      const SizedBox(height: 16.0),
+                      TextFormField(
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        controller: passwordController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          if (!user.validatePassword()) {
+                            return 'password is not valid';
+                          }
+                          return null;
+                        },
+                        obscureText: true,
+                        decoration: CustomTextFieldDecorator()
+                            .getInputDecorator('Password'),
+                      ),
+                      const SizedBox(height: 16.0),
+                      AnimatedBuilder(
+                          animation: controller.message,
+                          builder: (context, child) {
+                            return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                style: const TextStyle(
+                                  color: Colors.red,
+                                ),
+                                controller.message.value,
+                              ),
+                            );
+                          }),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: SizedBox(
+                              height: 50,
+                              child: ElevatedButton(
+                                style: CustomButtonStyle().getStyle(),
+                                onPressed: controller.login,
+                                child: const Text('Enter'),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
