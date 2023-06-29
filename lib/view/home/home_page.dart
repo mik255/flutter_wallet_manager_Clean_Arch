8ba697b6dde9b528e38bfba1867d16a901b99ba5
Mik255 <mikael20informatica@gmail.com>
2023-06-29 09:31:58 -0300

    home card

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_pluggy_connect/flutter_pluggy_connect.dart';
 import 'package:wallet_manager/shared/main_stances.dart';
 import 'package:wallet_manager/view/home/loadingPage.dart';
+import 'package:wallet_manager/view/home/transactions_page.dart';
 import 'package:wallet_manager/view/home/widgets/billing_item_widget.dart';
 import 'package:wallet_manager/view/home/widgets/category_item.dart';
 import 'package:wallet_manager/view/home/widgets/last_billing_grid_item.dart';
@@ -11,7 +12,7 @@ import 'package:wallet_manager/view/home/widgets/middle_info.dart';
 import 'package:wallet_manager/view/home/widgets/percentage_related.dart';
 import 'package:wallet_manager/view/home/widgets/perfil.dart';
 import '../styles/text_styles.dart';
-
+import 'dart:math';
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -19,20 +20,110 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   bool showPlugglyConnect = false;
+  late TabController _tabController;
+  bool menuOpened = false;
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
 
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              showPlugglyConnect = true;
-            });
-          },
-          child: const Icon(Icons.add),
+        floatingActionButton: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            // alignment: FractionalOffset.centerLeft,
+            // transform: Matrix4.identity()..scale(-1.0, 1),
+            AnimatedContainer(
+              alignment: Alignment.bottomRight,
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOut,
+              width: menuOpened?174:0,
+              height: menuOpened?156:0,
+              decoration: ShapeDecoration(
+                color: const Color(0xFFF5F6F9),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(17),
+                ),
+                shadows: const [
+                  BoxShadow(
+                    color: Color(0x3F000000),
+                    blurRadius: 4,
+                    offset: Offset(0, 4),
+                    spreadRadius: 0,
+                  )
+                ],
+              ),
+              child: menuOpened?FadeTransition(
+                opacity: Tween<double>(begin: 0, end: 1).animate(CurvedAnimation(
+                  parent: AnimationController(
+                    duration: const Duration(milliseconds: 1500),
+                    vsync: this,
+                  )..forward(),
+                  curve: Curves.easeInOut,
+                )),
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Wrap(
+                      runSpacing: 16,
+                      runAlignment: WrapAlignment.center,
+                      children: [
+                         InkWell(
+                           onTap: () {
+                             setState(() {
+                               showPlugglyConnect = true;
+                             });
+                           },
+                           child: Text(
+                            'Instituição financeira',
+                            style: CustomTextStyles().smallSubtitle,
+                        ),
+                         ),
+                         Text(
+                          'Conta Periódica',
+                          style: CustomTextStyles().smallSubtitle,
+                        ),
+                        Text(
+                          'Conta Agendada',
+                          style: CustomTextStyles().smallSubtitle,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ):Container(),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                const SizedBox(width: 150),
+                FloatingActionButton(
+                  onPressed: () {
+                   setState(() {
+                     menuOpened = !menuOpened;
+                   });
+                  },
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            ),
+          ],
         ),
         body: Builder(
             builder: (context) {
@@ -53,113 +144,104 @@ class _HomePageState extends State<HomePage> {
                   },
                 );
               }
-              return SingleChildScrollView(
-                child: Wrap(
-                  runSpacing: 20,
-                  children: [
-                    Container(
-                        decoration: const BoxDecoration(
+              return Column(
+                children: [
+                  Container(
+                      decoration: const BoxDecoration(
 
-                          image: DecorationImage(
-                            image: AssetImage('assets/home_background.png'),
-                            fit: BoxFit.cover,
-                          ),
+                        image: DecorationImage(
+                          image: AssetImage('assets/home_background.png'),
+                          fit: BoxFit.cover,
                         ),
-                        height: 300,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 16.0, vertical: 8),
-                          child: Column(
-                            children: [
-                              const PerfilWidget(),
-                              Expanded(
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    Container(
-                                      height: 100,
-                                      width: MediaQuery
-                                          .of(context)
-                                          .size
-                                          .width * 0.8,
-                                      padding: const EdgeInsets.symmetric(
-                                          vertical: 8.0, horizontal: 32),
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(10),
-                                        color: Colors.transparent,
-                                        border: Border.all(
-                                          color: Colors.black,
-                                          width: 2,
-                                        ),
+                      ),
+                      height: 300,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 16.0, vertical: 8),
+                        child: Column(
+                          children: [
+                            const PerfilWidget(),
+                            Expanded(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    height: 100,
+                                    width: MediaQuery
+                                        .of(context)
+                                        .size
+                                        .width * 0.8,
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 8.0, horizontal: 32),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Colors.transparent,
+                                      border: Border.all(
+                                        color: Colors.black,
+                                        width: 2,
                                       ),
-                                      child: Center(
-                                        child: Text(
-                                          '\$ 3.248,95',
-                                          style: CustomTextStyles().title
-                                              .copyWith(
-                                            color: Colors.black,
-                                          ),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        '\$ 3.248,95',
+                                        style: CustomTextStyles().title
+                                            .copyWith(
+                                          color: Colors.black,
                                         ),
                                       ),
                                     ),
-                                  ],
-                                ),
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
-                        )),
-                    Padding(
-                      padding:
-                      const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16),
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          children: [
-                            ...List.generate(
-                                4,
-                                    (index) => const CategoryItem())
+                            ),
                           ],
                         ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
+                      )),
+                  TabBar(
+                    controller: _tabController,
+                    tabs: const [
+                      Tab(text: 'Minhas Contas'),
+                      Tab(text: 'Transações'),
+                      Tab(text: 'Estatisticas'),
+                    ],
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding:
+                      const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16),
+                      child: TabBarView(
+                        physics: const NeverScrollableScrollPhysics(),
+                        controller: _tabController,
                         children: [
-                          Text(
-                            'Minhas Contas',
-                            style: CustomTextStyles().smallTitle.copyWith(
-                              color: Colors.black,
+                          // Conteúdo da Tab 1
+                          Padding(
+                            padding:
+                            const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16),
+                            child: SingleChildScrollView(
+                              child: Column(
+                                children: [
+                                  ...List.generate(
+                                      MainStances.plugglyService.getBankAccounts
+                                          .length,
+                                          (index) =>
+                                          BillingItemWidget(
+                                              bankAccount: MainStances.plugglyService
+                                                  .getBankAccounts.toList()[index]))
+                                ],
+                              ),
                             ),
                           ),
+                          // Conteúdo da Tab 2
+                          const Center(child: TransactionsPage()),
+                          // Conteúdo da Tab 3
+                          Center(child: Text('Conteúdo da Tab 3')),
                         ],
                       ),
                     ),
-                    Padding(
-                      padding:
-                      const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16),
-                      child: SingleChildScrollView(
-                        child: Column(
-                          children: [
-                            ...List.generate(
-                                MainStances.plugglyService.getBankAccounts
-                                    .length,
-                                    (index) =>
-                                    BillingItemWidget(
-                                        bankAccount: MainStances.plugglyService
-                                            .getBankAccounts.toList()[index]))
-                          ],
-                        ),
-                      ),
-                    ),
-                    const MiddleInfo(),
-                    LestBillingGridItem(),
-                    PercentageRelated(),
-                  ],
-                ),
+                  ),
+                ],
               );
             }
         ),
--- lib/view/home/widgets/billing_item_widget.dart
+++ lib/view/home/widgets/billing_item_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:wallet_manager/models/transaction.dart';
 import 'package:wallet_manager/shared/extensions/current_formate.dart';
+import 'package:wallet_manager/shared/extensions/formate_date.dart';
 
 import '../../../models/bank_account.dart';
 import '../../styles/container_decorators.dart';
@@ -14,10 +16,20 @@ class BillingItemWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 16.0),
-      decoration: const BoxDecoration(
-        color: Color(0xC3E6E9F1),
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-      ),
+      height: 238,
+      decoration: ShapeDecoration(
+          color: const Color(0xFFF5F6F9),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          shadows: const [
+            BoxShadow(
+              color: Color(0x3F000000),
+              blurRadius: 4,
+              offset: Offset(0, 4),
+              spreadRadius: 0,
+            )
+          ]),
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
         child: Column(
@@ -71,37 +83,151 @@ class BillingItemWidget extends StatelessWidget {
               ],
             ),
             const SizedBox(height: 16),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ...bankAccount.balanceTypes.map((e) => Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '${e.balanceType.getName} :',
+                            style: const TextStyle(
+                              color: Color(0xFF505869),
+                              fontSize: 14,
+                              fontFamily: 'Roboto',
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          Text(
+                            e.balance.toCurrencyString(withSymbol: false),
+                            style: const TextStyle(
+                              color: Color(0xFF0C1425),
+                              fontSize: 14,
+                              fontFamily: 'Roboto',
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      )),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-                ...bankAccount.balanceTypes.map((e) => Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      '${e.balanceType.getName} :',
-                      style: const TextStyle(
-                        color: Color(0xFF505869),
-                        fontSize: 14,
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.w500,
+class BillingItemTransactions extends StatelessWidget {
+  const BillingItemTransactions({super.key, required this.transaction});
+
+  final Transaction transaction;
+
+  @override
+  Widget build(BuildContext context) {
+    String title = transaction.name;
+    String subtitle = '';
+    try {
+      title = transaction.name.split('-')[0];
+      subtitle = transaction.name.split('-')[1];
+    } catch (e) {}
+
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  clipBehavior: Clip.antiAlias,
+                  height: 50,
+                  width: 50,
+                  decoration: ContainerDecorators().getBoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Container(
+                      clipBehavior: Clip.antiAlias,
+                      height: 40,
+                      width: 40,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
                       ),
-                    ),
-                    Text(
-                      e.balance.toCurrencyString(withSymbol: false),
+                      child: const Center(
+                          child: Icon(
+                        Icons.add_card_outlined,
+                        color: Colors.black,
+                      ))),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Text(
+                            title,
+                            maxLines: 3,
+                            style: CustomTextStyles()
+                                .smallSubtitle
+                                .copyWith(fontWeight: FontWeight.w500),
+                          ),
+                          Spacer(),
+                          Text(
+                            DateTime.parse(transaction.date).formatDate(),
+                            style: const TextStyle(
+                              color: Color(0xFF505869),
+                              fontSize: 14,
+                              fontFamily: 'Roboto',
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                      if (subtitle.isNotEmpty)
+                        Text(
+                          subtitle,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
                           style: const TextStyle(
-                            color: Color(0xFF0C1425),
+                            color: Color(0xFF505869),
                             fontSize: 14,
                             fontFamily: 'Roboto',
-                            fontWeight: FontWeight.w500,
+                            fontWeight: FontWeight.w400,
                           ),
                         ),
-                  ],
-                )),
+                      Text(
+                        transaction.amount
+                            .abs()
+                            .toCurrencyString(withSymbol: false),
+                        style: const TextStyle(
+                          color: Color(0xFF505869),
+                          fontSize: 14,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
+            ),
+            const Divider(
+              color: Colors.black12,
             )
           ],
         ),
