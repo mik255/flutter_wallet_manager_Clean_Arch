b573e514f5329b14156aea2589bbf8f983e70163
Mik255 <mikael20informatica@gmail.com>
2023-06-29 12:35:09 -0300

    refactor

--- lib/app/view_models/user_viewmodel.dart
+++ lib/app/view_models/user_viewmodel.dart
@@ -1,27 +1,35 @@
 import 'dart:convert';
 import 'package:flutter/cupertino.dart';
-import '../../../data/abstract_preferences_helper.dart';
-import '../../../models/user.dart';
-import '../../../services/auth/auth_service_helper.dart';
+import '../../data/abstract_preferences_helper.dart';
+import '../../models/user.dart';
+import '../../services/auth/auth_service_helper.dart';
 
-class LoginViewModel {
+class UserViewModel {
   User? user;
   IPreferencesHelper helper;
   ValueNotifier<String> message = ValueNotifier('');
   var formKey = GlobalKey<FormState>();
 
-  LoginViewModel({
+  UserViewModel({
     required this.helper,
   });
 
   Future<bool> login(AuthServiceHelper authServiceHelper) async {
     try {
-      user = await authServiceHelper.auth();
+      user = await authServiceHelper.singIn();
       await helper.saveData('user', jsonEncode(user!.toMap()));
       return true;
     } catch (e) {
       message.value = e.toString();
       return false;
+    }
+  }
+  singOut(AuthServiceHelper authServiceHelper) async {
+    try {
+      await authServiceHelper.singOut();
+      await helper.deleteData('user');
+    } catch (e) {
+      message.value = e.toString();
     }
   }
 }
